d413436e1710ef4bb445cb27bd3225c610201893
naresh <nareshsivani2001@gmail.com>
2021-08-24 16:35:13 +0530

    SuperHero

--- app/src/main/java/com/example/superhero/MainActivity.java
+++ app/src/main/java/com/example/superhero/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.Menu;
@@ -22,8 +23,15 @@ import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -32,7 +40,7 @@ import java.util.List;
 import static android.view.View.GONE;
 import static com.example.superhero.GetHeros.HERO_REQUEST_URL;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<SuperHero>>, Serializable {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<SuperHero>>, Serializable, NavigationView.OnNavigationItemSelectedListener {
     private static SuperHero currentSuperHero;
     HeroAdapter adapter;
     private TextView mEmptyStateTextView;
@@ -40,6 +48,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     Parcelable state;
     ArrayList<SuperHero> superHeroesAll;
     String searchStr;
+    ArrayList<SuperHero> maleSuperHero, femaleSuperHero, nonBinarySuperHero;
+    ArrayList<SuperHero> dcComics, marvelComics, otherPublications;
+    private DrawerLayout drawer;
+    public List<SuperHero> superHeroListFiltered;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +71,25 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             mEmptyStateTextView = (TextView) findViewById(R.id.emptyView);
             heroListView.setEmptyView(mEmptyStateTextView);
 
+            maleSuperHero = new ArrayList<>();
+            femaleSuperHero = new ArrayList<>();
+            nonBinarySuperHero = new ArrayList<>();
+            dcComics = new ArrayList<>();
+            marvelComics = new ArrayList<>();
+
+            otherPublications = new ArrayList<>();
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            drawer = findViewById(R.id.drawer_layout);
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(MainActivity.this);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                    R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
             LoaderManager loaderManager = getLoaderManager();
-
-
             loaderManager.initLoader(1, null, this);
 
         }
@@ -76,7 +105,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<SuperHero>> onCreateLoader(int id, Bundle args) {
-
         return new HeroLoader(this, HERO_REQUEST_URL);
     }
 
@@ -95,17 +123,33 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
         adapter = new HeroAdapter(superHeroes, this);
         heroListView.setAdapter(adapter);
+//        if(!superHeroesAll.isEmpty() && maleSuperHero.size() == 0) {
+//            for (SuperHero x : superHeroesAll) {
+//                if ((x.getGender().toLowerCase()).contains("Male".toLowerCase())) {
+//                    maleSuperHero.add(x);
+//                } else if ((x.getGender().toLowerCase()).contains("Female".toLowerCase())) {
+//                    femaleSuperHero.add(x);
+//                } else {
+//                    nonBinarySuperHero.add(x);
+//                }
+//                if ((x.getPublisher().toLowerCase()).contains("DC comic".toLowerCase())) {
+//                    dcComics.add(x);
+//                } else if ((x.getGender().toLowerCase()).contains("marvel comic".toLowerCase())) {
+//                    marvelComics.add(x);
+//                } else {
+//                    otherPublications.add(x);
+//                }
+//            }
+//        }
         if(state!=null){
             heroListView.onRestoreInstanceState(state);
         }
 
     }
 
-
     @Override
     public void onLoaderReset(Loader<List<SuperHero>> loader) {
         adapter = new HeroAdapter(superHeroesAll, this);
-
     }
 
     @Override
@@ -129,10 +173,41 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         return true;
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.allInNav) {
+            adapter.getFilter().filter("");
+        }
+
+        if (item.getItemId() == R.id.maleInNav) {
+            adapter.getFilter().filter("male");
+        }
+
+        else if (item.getItemId() == R.id.femaleInNav) {
+            adapter.getFilter().filter("female");
+        }
+
+        else if (item.getItemId() == R.id.nonBinaryInNav) {
+            adapter.getFilter().filter("binary");
+        }
+
+        else if (item.getItemId() == R.id.dcComicsInNav) {
+            adapter.getFilter().filter("dc comics");
+        }
+
+        else if (item.getItemId() == R.id.marvelComicsInNav) {
+            adapter.getFilter().filter("marvel comics");
+        }
+
+        else if (item.getItemId() == R.id.otherComicsInNav) {
+            adapter.getFilter().filter("other comics");
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     public class HeroAdapter extends BaseAdapter implements Filterable {
         private List<SuperHero> superHeroList;
-        private List<SuperHero> superHeroListFiltered;
         private Context context;
 
         public HeroAdapter(List<SuperHero> superHeroes, Context context) {
@@ -207,26 +282,68 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             return view;
         }
 
+        public void refreshAdapter(){
+            notifyDataSetChanged();
+        }
+
         @Override
         public Filter getFilter() {
             Filter filter = new Filter() {
                 @Override
                 protected FilterResults performFiltering(CharSequence constraint) {
                     FilterResults filterResults = new FilterResults();
-                    if(constraint == null || constraint.length() == 0) {
-                        filterResults.count = superHeroList.size();
-                        filterResults.values = superHeroList;
-                    }else{
-                        searchStr = constraint.toString().toLowerCase();
-                        List<SuperHero> resultData = new ArrayList<>();
-                        for(SuperHero superHero:superHeroList){
-                            if(superHero.getName().toLowerCase().contains(searchStr)){
-                                resultData.add(superHero);
-                                filterResults.count = resultData.size();
-                                filterResults.values = resultData;
+                    AsyncTask.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(constraint == null || constraint.length() == 0) {
+                                filterResults.count = superHeroList.size();
+                                filterResults.values = superHeroList;
+                            }else{
+                                searchStr = constraint.toString().toLowerCase();
+                                List<SuperHero> resultData = new ArrayList<>();
+                                for(SuperHero superHero:superHeroList) {
+                                    if (searchStr.contains("binary")) {
+                                        if (!superHero.getGender().toLowerCase().contains("male")) {
+                                            resultData.add(superHero);
+                                            filterResults.count = resultData.size();
+                                            filterResults.values = resultData;
+                                        }
+                                    } else if (searchStr.contains("male")) {
+                                        if (superHero.getGender().toLowerCase().equals(searchStr)) {
+                                            resultData.add(superHero);
+                                            filterResults.count = resultData.size();
+                                            filterResults.values = resultData;
+                                        }
+                                    } else if (searchStr.toLowerCase().contains("dc comics")   || searchStr.toLowerCase().contains("other comics")) {
+                                        if (superHero.getPublisher().toLowerCase().contains("dc")) {
+                                            resultData.add(superHero);
+                                            filterResults.count = resultData.size();
+                                            filterResults.values = resultData;
+                                        }
+                                    } else if (searchStr.toLowerCase().contains("marvel comics")) {
+                                        if (superHero.getPublisher().toLowerCase().contains("marvel")) {
+                                            resultData.add(superHero);
+                                            filterResults.count = resultData.size();
+                                            filterResults.values = resultData;
+                                        }
+                                    }
+
+                                    else if(searchStr.toLowerCase().contains("other comics")){
+                                        if(superHero.getPublisher().toLowerCase().contains("marvel") || superHero.getPublisher().toLowerCase().contains("dc")){
+                                            resultData.add(superHero);
+                                            filterResults.count = resultData.size();
+                                            filterResults.values = resultData;
+                                        }
+                                    }
+                                    else if(superHero.getName().toLowerCase().contains(searchStr)){
+                                        resultData.add(superHero);
+                                        filterResults.count = resultData.size();
+                                        filterResults.values = resultData;
+                                    }
+                                }
                             }
                         }
-                    }
+                    });
                     return filterResults;
                 }
 
@@ -253,8 +370,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             };
             return filter;
         }
-
-
 
         private int getpowerColor(double power) {
             int powerColorResourceId = 0;
